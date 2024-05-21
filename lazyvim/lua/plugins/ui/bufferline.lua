fe2bc82f2a1838b45ccf6cab4ad0a6f700014c10
Devarsh Patel <devarshptl@Devarshs-MacBook-Pro.local>
2024-05-21 17:24:20 -0500

    initial commit

---
+++ lazyvim/lua/plugins/ui/bufferline.lua
@@ -0,0 +1,10 @@
+return {
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      diagnostics = "nvim_lsp",
+      indicator = { style = "none" },
+      separator_style = { "", "" },
+    },
+  },
+}
---
+++ lazyvim/lua/plugins/ui/colorscheme.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      -- transparent_background = true,
+      term_colors = true,
+      custom_highlights = function(colors)
+        return {
+          DashboardHeader = { fg = colors.mauve },
+          NeoTreeDimText = { fg = colors.overlay0 },
+          NeoTreeMessage = { fg = colors.overlay0 },
+          NeoTreeDotFile = { fg = colors.overlay0 },
+        }
+      end,
+    },
+  },
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+    opts = {
+      colors = {
+        gray = "#5c6370",
+      },
+      highlights = {
+        DashboardHeader = { fg = "${green}" },
+        DashboardIcon = { fg = "${yellow}" },
+        DashboardDesc = { fg = "${blue}" },
+        DashboardKey = { fg = "${orange}" },
+        DashboardFooter = { fg = "${purple}" },
+        PmenuSel = { bg = "${gray}" },
+        CmpItemMenu = { fg = "${red}" },
+        NeoTreeDimText = { fg = "${gray}" },
+        NeoTreeMessage = { fg = "${gray}" },
+        NeoTreeDotFile = { fg = "${gray}" },
+        Visual = { bg = "${gray}" },
+        WhichKeyFloat = { bg = "${bg}" },
+        StatusLine = { bg = "${bg_statusline}" },
+      },
+    },
+  },
+}
