99d37df9d01d5ef49e27ca5ffaa445787aceef96
Devarsh Patel <devarshptl@Devarshs-MacBook-Pro.local>
2024-05-21 17:24:20 -0500

    initial commit

---
+++ lazyvim/lua/plugins/lsp.lua
@@ -0,0 +1,48 @@
+require("lspconfig.ui.windows").default_options.border = "rounded"
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        cssls = {},
+        denols = {},
+        vtsls = {
+          settings = {
+            javascript = {
+              preferences = {
+                importModuleSpecifier = "non-relative",
+              },
+            },
+            typescript = {
+              preferences = {
+                importModuleSpecifier = "non-relative",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "akinsho/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = true,
+  },
+  {
+    "mistricky/codesnap.nvim",
+    build = "make build_generator",
+    opts = {
+      watermark = "",
+      code_font_family = "SauceCodePro Nerd Font",
+    },
+    keys = {
+      { "<leader>cs", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+      { "<leader>cS", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+    },
+  },
+}
