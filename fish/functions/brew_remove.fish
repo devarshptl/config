a85991b5aae79eb11ff2973d4f77dda318ee1ec1
Devarsh Patel <dev@devpat.local>
2024-07-20 19:48:21 -0500

    Format fish functions, clean brew cache after upgrade
    - Format fish functions
    - Append `brew_clean` inside `brew_upgrade'

--- fish/functions/brew_remove.fish
+++ fish/functions/brew_remove.fish
@@ -1,4 +1,3 @@
 function brew_remove --wraps='brew uninstall --zap -f' --description 'alias brew_remove brew uninstall --zap -f'
-  brew uninstall --zap -f $argv
-        
+    brew uninstall --zap -f $argv
 end
--- fish/functions/history_clear.fish
+++ fish/functions/history_clear.fish
@@ -1,4 +1,3 @@
 function history_clear --wraps='echo "yes" | history clear' --description 'alias history_clear echo "yes" | history clear'
-  echo "yes" | history clear $argv
-        
+    echo yes | history clear $argv
 end
