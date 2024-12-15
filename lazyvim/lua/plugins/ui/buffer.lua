6e792d4e7261cfa8b0d51ceec037fb3a5b124b15
Devarsh Patel <dev@Devarshs-MacBook-Pro.local>
2024-12-14 23:05:33 -0500

    Update LazyVim to 14.x
    - Update buffer to use `blink.cmo`

--- lazyvim/lua/plugins/ui/buffer.lua
+++ lazyvim/lua/plugins/ui/buffer.lua
@@ -1,36 +1,20 @@
-local cmp = require("cmp")
-
 return {
   -- Mason
   { "williamboman/mason.nvim", opts = { ui = { border = "rounded" } } },
-  -- nvim-cmp
   {
-    "hrsh7th/nvim-cmp",
+    "saghen/blink.cmp",
     opts = {
-      window = {
-        completion = {
+      completion = {
+        menu = {
           border = "rounded",
-          winblend = 0,
           winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
         },
+
         documentation = {
-          border = "rounded",
-          winblend = 0,
+          window = {
+            border = "rounded",
+          },
         },
-      },
-      view = {
-        docs = {
-          auto_open = false,
-        },
-      },
-      mapping = {
-        ["<C-g>"] = function()
-          if cmp.visible_docs() then
-            cmp.close_docs()
-          else
-            cmp.open_docs()
-          end
-        end,
       },
     },
   },
