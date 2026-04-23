78ae40b7f9e39b522f55e7134ebe17b6141f1779
Devarsh Patel <devarshptl@Devarshs-MacBook-Pro.local>
2026-04-23 01:29:05 -0500

    Add NvChad

---
+++ fish/functions/nvchad.fish
@@ -0,0 +1,3 @@
+function nvchad --wraps='NVIM_APPNAME=nvchad nvim' --description 'alias nvchad NVIM_APPNAME=nvchad nvim'
+    NVIM_APPNAME=nvchad nvim $argv
+end
---
+++ nvchad/lua/chadrc.lua
@@ -0,0 +1,32 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+
+  theme = "catppuccin",
+	transparency = true
+  -- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+-- M.nvdash = { load_on_startup = true }
+-- M.ui = {
+--       tabufline = {
+--          lazyload = false
+--      }
+-- }
+
+ M.term = {
+   float = {
+     row = 0.05, col = 0.1,
+     width = 0.8, height = 0.8,
+     border = "single",
+   },
+ }
+return M
